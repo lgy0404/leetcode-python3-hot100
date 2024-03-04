e1602e8831e771b149f518c77544ed0afc61b08c
lgy0404 <2528379691@qq.com>
2024-03-04 15:06:25 +0800

    2403041505-0001哈希表复习

--- vscode/1.两数之和.py
+++ vscode/1.两数之和.py
@@ -5,14 +5,24 @@
 #
 
 # @lc code=start
+# 哈希表解题
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #哈希表解题
         hashtable = {}
-        for index,val in enumerate(nums):
+        for index, val in enumerate(nums):
             if target - val not in hashtable:
                 hashtable[val] = index
             else:
-                return [hashtable[target - val],index]
+                return [index, hashtable[target - val]]
+        
+
+# # 暴力解法
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         n = len(nums)
+#         for i in range(0, n):
+#             for j in range(i + 1, n):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
 # @lc code=end
 
